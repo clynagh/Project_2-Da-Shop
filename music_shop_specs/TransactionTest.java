dd1bdf8be2f64b1dbcba41abde542441b57d7f33
Craig Lynagh <craiglynagh@gmail.com>
2017-08-16 15:55:27 +0100

    Transactions test pass with new wallet of payment types

--- music_shop_specs/TransactionTest.java
+++ music_shop_specs/TransactionTest.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import music_shop.*;
 import behaviours.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class TransactionTest {
 
@@ -16,17 +19,35 @@ public class TransactionTest {
   MusicPlayer boombox;
   MusicPlayer cdPlayer;
   MusicPlayer turntable;
+  // Wallet jimwallet;
+  ArrayList<HashMap<PaymentType, Integer>> jimwallet;
+  HashMap<PaymentType, Integer> cashAmount;
+  HashMap<PaymentType, Integer> creditCardAmount;
+  HashMap<PaymentType, Integer> debitCardAmount;
   
   @Before
   public void before(){
     shop = new Shop("Obsolescence");
-    // customer = new Customer("Jim", PaymentType.CASH);
     cassette = new MusicFormat(202, "Sonic Youth", "Sonic Death", MusicType.CASSETTE, 10, 20);
     compactDisc = new MusicFormat(203, "Violent Femmes", "American People", MusicType.COMPACT_DISC, 8, 16);
     record = new MusicFormat(204, "The Doors", "Morrison Hotel", MusicType.RECORD, 15, 30);
     boombox = new MusicPlayer(101, "Sony megabass", PlayerType.BOOMBOX, 100, 200);
     cdPlayer = new MusicPlayer(102, "Cambridge Audio", PlayerType.CD_PLAYER, 150, 300);
     turntable = new MusicPlayer(103, "Project", PlayerType.TURNTABLE, 200, 400);
+    jimwallet = new ArrayList<HashMap<PaymentType, Integer>>();
+
+    cashAmount = new HashMap<PaymentType, Integer>();
+    cashAmount.put(PaymentType.CASH, 150);
+    creditCardAmount = new HashMap<PaymentType, Integer>();
+    creditCardAmount.put(PaymentType.CREDIT_CARD, 450);
+    debitCardAmount = new HashMap<PaymentType, Integer>();
+    debitCardAmount.put(PaymentType.DEBIT_CARD, 250);
+
+    jimwallet.add(cashAmount);
+    jimwallet.add(creditCardAmount);
+    jimwallet.add(debitCardAmount);
+
+    customer = new Customer("Jim", jimwallet);
   }
 
   @Test
@@ -64,7 +85,6 @@ public class TransactionTest {
   @Test
   public void canMakeRefund(){
     customer.addItemToPurchases(record);
-    assertEquals(1, customer.countItems());
     Transactions.makeRefund(customer, shop, record);
     assertEquals(0, customer.countItems());
     assertEquals(1, shop.countItems());
